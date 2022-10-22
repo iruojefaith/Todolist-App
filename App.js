d982676c5e6f7ffeb25fb79f7b02eb00269caf2d
iruojefaith <fumsy240@gmail.com>
2022-10-22 15:06:35 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,46 @@
+document.querySelector('#push').onclick = function(){
+
+    if(document.querySelector('#newtask textarea').value.length == 0){
+        alert("Please Enter a Task")
+    }
+
+    else{
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task" id="edit">
+                <span id="taskname" class="edit">
+                    ${document.querySelector('#newtask textarea').value}
+                    ${document.querySelector('#newtask textarea').value = ''}
+                </span>
+
+                <button class="delete">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+
+            </div>
+        `;
+
+        let current_tasks = document.querySelectorAll(".delete");
+        for(let i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+
+        let current_edit = document.querySelectorAll("#edit");
+        for(let i=0; i<current_edit.length; i++){
+            current_edit[i].onclick = function(){
+                this.contentEditable = true;
+            }
+        }
+    }
+}
+
+// const edit_button = document.getElementById("edit-button");
+// const end_button = document.getElementById("end-editing");
+
+// edit_button.addEventListener("click", function() {
+//   paragraph.contentEditable = true;
+//   paragraph.style.backgroundColor = "#dddbdb";
+// } );
+
+
